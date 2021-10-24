2144cfcb52770cf076a92f08365ad2050240d072
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2021-10-24 19:35:18 +0200

    write message completed

--- monolith/tests/test_views_messages.py
+++ monolith/tests/test_views_messages.py
@@ -1,28 +1,51 @@
-import unittest
+import pytest
 from monolith import app
 from monolith.database import Message, User, db
-from monolith.forms import MessageForm
 
-app.testing = True
+class TestViewsMessages():
 
-class TestViewsMessages(unittest.TestCase):
+    def test_post_draft_added_non_auth(self, test_client):
+        draft_body= 'test_draft'
+        data = { 'body_message': draft_body }
+        response = test_client.post('/draft', data=data, follow_redirects=True)
+        assert response.status_code == 401
+        assert b'You must be logged in' in response.data
 
-    def setUp(self):
-        app.config['WTF_CSRF_ENABLED'] = False 
+    def test_get_draft_non_auth(self, test_client):
+        response = test_client.get('/draft')
+        assert response.status_code == 200
+        assert b'Hi Anonymous' in response.data
 
-    def test_draft(self):
-        tested_app = app.test_client()
-        TESTUSER = ('Admin', 'Admin')
-        
-        with app.test_request_context('/draft'):
-            old_len = db.session.query(Message).count()
-            user = db.session.query(User).filter(User.firstname == TESTUSER[0] and User.lastname == TESTUSER[1]).first()
-            draft = Message(body_message='test draft', id_sender=user.id)
-            form = MessageForm(obj=draft)
-            print(form.body_message.data)
-            response = tested_app.post('/draft', data=form.data, follow_redirects=True)
+    def test_post_draft_added_auth(self, test_client):
+        admin_user = { 'email': 'example@example.com', 'password': 'admin' }
+        draft_body= 'test_draft'
+ 
+        response = test_client.post('/login', data=admin_user, follow_redirects=True)
+        assert response.status_code == 200       
 
-            self.assertEqual(old_len, db.session.query(Message).count() - 1)
+        old_len = db.session.query(Message).count()
+
+        data = { 'body_message': draft_body }
+        response = test_client.post('/draft', data=data, follow_redirects=True)
+        assert response.status_code == 200       
+
+        # Check that the message was added to the table
+        assert old_len + 1  == db.session.query(Message).count()
+
+        # Check that informations inside the database are correct
+        user = db.session.query(User).filter(User.email == 'example@example.com').first()
+        draft_db = db.session.query(Message).order_by(Message.id_message.desc()).first()
+        assert draft_db.id_message == old_len + 1
+        assert draft_db.id_sender == user.id
+        assert draft_db.body_message == draft_body
+
+    def test_get_draft_auth(self, test_client):
+        response = test_client.get('/draft')
+        assert response.status_code == 200
+        assert b'Message' in response.data
+        assert b'submit' in response.data
+
+
 
 
 
--- monolith/views/messages.py
+++ monolith/views/messages.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request, jsonify
+from flask import Blueprint, redirect, render_template, request, jsonify, abort
 
 from monolith.auth import current_user
 from monolith.database import Message, db
@@ -8,13 +8,14 @@ messages = Blueprint('messages', __name__)
 
 @messages.route('/draft', methods=['POST', 'GET'])
 def draft():
-    form = MessageForm()
-    print('start')
 
+    form = MessageForm()
     if request.method == 'POST':
-        print(form.data)
+
+        if (current_user.get_id() == None):
+            abort(401, description='You must be logged in to draft a message')
+
         if form.validate_on_submit():
-            print('vos')
             new_draft = Message()
             form.populate_obj(new_draft)
             new_draft.id_sender = current_user.get_id()
@@ -24,14 +25,15 @@ def draft():
     elif request.method == 'GET':
         # form.submit.label.text = 'Save as Draft'
         return render_template('create_message.html', form=form)
-    else:
-        raise RuntimeError('This should not happen!')
 
 @messages.route('/read_message/<int:id>', methods=['GET'])
 def read_message(id):
+    result = ''
     if request.method == 'GET':
         msg = db.session.query(Message).filter(Message.id_message == id).first()
-        return jsonify(id=msg.id_message, body=msg.body_message, sender=msg.id_sender)
+        result = jsonify(id=msg.id_message, body=msg.body_message, sender=msg.id_sender)
+
+    return result
 
 
 
