802f5ac56b0cdc4ec58aba71166ec1f4285cc540
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2021-10-28 16:05:36 +0200

    monolith/views/read_message.py refactored

--- monolith/views/read_message.py
+++ monolith/views/read_message.py
@@ -17,7 +17,6 @@ def read_messages(id):
     sender_id = mess.id_sender
     mess_text = mess.body_message
     date_receipt = mess.date_of_send
-    sender_email = UserModel.get_user_info_by_id(sender_id)
 
     #some controls to check if user is allowed to read the message or not
     if (mess.is_arrived == 1):
@@ -27,11 +26,14 @@ def read_messages(id):
     elif (current_user.get_id() != mess.id_sender):
              abort(401,description='You are not allowed to read this message')
 
-    if sender_email is not None:
-        sender = sender_email.email
-    else:
-        sender = "Anonymous"
-    return render_template("read_select_message.html", mess_text = mess_text, sender=sender,date_receipt=date_receipt)
+    sender_email = ""
+    try:
+        sender = UserModel.get_user_info_by_id(sender_id)
+        sender_email = sender.email
+    except NotExistingUser:
+        sender_email = "Anonymous"
+    
+    return render_template("read_select_message.html", mess_text = mess_text, sender=sender_email,date_receipt=date_receipt)
 
 
 
