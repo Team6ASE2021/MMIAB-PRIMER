f219abe2450808ac95675dbf6df294fed95ba8bb
moru46 <morucci46@gmail.com>
2021-10-30 07:27:29 -0400

    forwarded messages implemented

---
+++ monolith/tests/views/test_views_forward.py
@@ -0,0 +1,32 @@
+import pytest
+from flask_login import logout_user
+from monolith import app
+from monolith.auth import current_user
+from monolith.database import Message, User, db
+from monolith.forms import delivery_format
+from datetime import datetime
+from monolith.classes.message import MessageModel
+
+@pytest.mark.usefixtures('clean_db_and_logout', 'messages_setup')
+class TestViewsForward():
+
+    def test_forward_mess_not_auth(self,test_client):
+        test_client.get('/logout',follow_redirects=True)
+        response = test_client.get('/forwarding/1',follow_redirects=True)
+        assert response.status_code == 401
+        assert b'You must be logged in to forward a message' in response.data
+
+    def test_forward_mess_auth(self,test_client):
+        test_client.get('/logout',follow_redirects=True)
+        admin_user = { 'email': 'example@example.com', 'password': 'admin' }
+        response = test_client.post('/login', data=admin_user, follow_redirects=True)
+        assert response.status_code == 200
+
+        response = test_client.get('/forwarding/10',follow_redirects=True)
+        assert response.status_code == 200
+
+
+
+
+
+
