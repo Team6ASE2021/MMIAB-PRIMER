1e5492e9f897e18bf85bc45f4c336b6e6f4babdd
Niccolò Piazzesi <npiazzesi@gmail.com>
2021-11-06 15:32:26 +0100

    added logging

--- monolith/background.py
+++ monolith/background.py
@@ -1,7 +1,9 @@
+import logging
 import random
 
 from celery import Celery
 from celery.schedules import crontab
+from celery.utils.log import get_task_logger
 
 from monolith.classes.lottery import LotteryModel
 from monolith.classes.message import MessageModel
@@ -15,7 +17,7 @@ celery = Celery(__name__, backend=BACKEND, broker=BROKER)
 TaskBase = celery.Task
 
 
-class ContextTask(TaskBase):
+class ContextTask(TaskBase):  # pragma: no cover
     def __call__(self, *args, **kwargs):
         global _APP
         # lazy init
@@ -39,6 +41,8 @@ celery.conf.beat_schedule = {
     },
 }
 
+logger = get_task_logger(__name__)
+
 
 @celery.task
 def test():  # pragma: nocover
@@ -52,7 +56,10 @@ def lottery_draw():
 
 
 def _lottery_draw():
+    logger.log(logging.INFO, "Drawing next lottery winners...")
     winner = random.randint(1, 50)
+    logger.log(logging.INFO, f"Winning number: {winner}")
+
     participants = LotteryModel.get_participants_with_choices()
     winners = list(
         map(
@@ -60,7 +67,13 @@ def _lottery_draw():
             filter(lambda u: u.choice == winner, participants),
         )
     )
+
+    logger.log(logging.INFO, "Adding points to winners...")
+
     for winner in winners:
         UserModel.update_points_to_user(winner, 1)
 
+    logger.log(logging.INFO, "Cleaning up lottery participants...")
+
     LotteryModel.reset_lottery()
+    logger.log(logging.INFO, "Table reset done, waiting for next lottery")
