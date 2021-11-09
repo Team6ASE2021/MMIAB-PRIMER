e2f1e28b4db7c5f19ee7f17cd5b071ffb524b0bb
Niccolò Piazzesi <npiazzesi@gmail.com>
2021-11-09 08:31:36 +0100

    removed trailing backlash in celery script [skip ci]

--- celery.sh
+++ celery.sh
@@ -5,4 +5,4 @@ mkdir -p /var/run/celery /var/log/celery
 exec  celery -A monolith.background worker \
              -B --loglevel=info \
              --logfile=/var/log/celery/worker.log \
-             --statedb=/var/run/celery/worker-example@%h.state \
+             --statedb=/var/run/celery/worker-example@%h.state
