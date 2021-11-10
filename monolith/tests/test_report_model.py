2e1bb41fabcf7c5312e0014be484096b82ba0c86
Niccolò Piazzesi <npiazzesi@gmail.com>
2021-11-10 13:05:02 +0100

    added tests

--- monolith/tests/test_report_model.py
+++ monolith/tests/test_report_model.py
@@ -1,21 +1,27 @@
-import datetime
 import pytest
-import string
-from monolith.classes.message import MessageModel, ContentFilter, NotExistingMessageError
-from monolith.classes.user import UserModel
+
 from monolith.classes.report import ReportModel
-from monolith.database import db, Report
+from monolith.database import db
+from monolith.database import User
 
-@pytest.mark.usefixtures('clean_db_and_logout')
+
+@pytest.mark.usefixtures("clean_db_and_logout")
 class TestReport:
-
     def test_add_report(self):
-        res = ReportModel.add_report(1,2)
+        res = ReportModel.add_report(1, 2)
 
         assert res == True
 
     def test_add_already_report(self):
-        res = ReportModel.add_report(1,2)
-        res = ReportModel.add_report(1,2)
+        res = ReportModel.add_report(1, 2)
+        res = ReportModel.add_report(1, 2)
 
         assert res == False
+
+    def test_report_trigger_ban(self):
+        for i in range(1, 11):
+            ReportModel.add_report(1, i)
+
+        assert db.session.query(User).first().is_banned
+        db.session.query(User).filter(User.id == 1).update({User.is_banned: False})
+        db.session.commit()
